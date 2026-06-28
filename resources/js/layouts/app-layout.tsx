1e7f7fccc5d5113fff11d9012bcc8b798bbfc393
maumans <mansaremaurice100@gmail.com>
2026-06-28 23:09:09 +0000

    save

--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -1,5 +1,6 @@
 import AppLayoutTemplate from '@/layouts/app/app-sidebar-layout';
 import { RealtimeNotifications } from '@/components/realtime-notifications';
+import { useFlashToast } from '@/hooks/use-flash-toast';
 import type { BreadcrumbItem } from '@/types';
 
 export default function AppLayout({
@@ -9,6 +10,8 @@ export default function AppLayout({
     breadcrumbs?: BreadcrumbItem[];
     children: React.ReactNode;
 }) {
+    useFlashToast();
+
     return (
         <>
             <RealtimeNotifications />
